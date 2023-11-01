53049d17ec7ddb8d18803d524ffd6b1b67938b5f
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-11-01 17:47:08 +0700

    Tutorial 6

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
